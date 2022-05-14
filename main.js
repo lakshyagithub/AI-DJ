f0ca678e325ba0e0bc10c28294a19bb94f297d07
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-05-14 19:33:45 +0530

    Update

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
+song="";
+function preload() {
+    song = loadSound("music.mp3");
+}
 function setup() {
     canvas = createCanvas(510, 510);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+  image(video, 0, 0, 510, 510);
+}
+function play1() {
+  song.play();
 }
